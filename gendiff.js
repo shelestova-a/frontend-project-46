17f517ae70a4b5fe6f4174c48880924d053a206f
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-03-22 17:58:16 +0100

    update gendiff.js with arguments

--- gendiff.js
+++ gendiff.js
@@ -4,5 +4,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
   .parse()
