89deaae711f688cc5595c3d7e506f29b9319efbe
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-03-22 17:26:18 +0100

    add gendiff.js, package.json, package-lock.json

---
+++ gendiff.js
@@ -0,0 +1,8 @@
+import { Command } from 'commander';
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .parse()
