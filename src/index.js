ae7945076ca112454160a1e7bfee9979ce2f3e19
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-03-24 23:15:16 +0100

    modify index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import fs from 'fs';
 
 import path from 'path';
 
+import yaml from 'js-yaml';
+
 const getParsedObject = (filepath) => {
   const pathAbsolute = path.resolve(filepath);
   const content = fs.readFileSync(pathAbsolute, 'utf-8');
-  const objectParsed = JSON.parse(content);
+  const exten = filepath.split('.').pop();
+  let objectParsed;
+  if (exten.toLowerCase() === 'json') {
+    objectParsed = JSON.parse(content);
+  } else if ((exten.toLowerCase() === 'yaml') || (exten.toLowerCase() === 'yml')) {
+    objectParsed = yaml.load(content);
+  }
   return objectParsed;
 };
 
