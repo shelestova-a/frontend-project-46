3c3a5e2fd0e68d7bf2b5fe99b8421679d5a6e656
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-03-27 20:21:14 +0200

    modify index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import fs from 'fs';
 
 import path from 'path';
 
-import yaml from 'js-yaml';
+import getParsedObject from './parser.js';
 
-const getParsedObject = (filepath) => {
+const getParsedData = (filepath) => {
   const pathAbsolute = path.resolve(filepath);
   const content = fs.readFileSync(pathAbsolute, 'utf-8');
   const exten = filepath.split('.').pop();
-  let objectParsed;
-  if (exten.toLowerCase() === 'json') {
-    objectParsed = JSON.parse(content);
-  } else if ((exten.toLowerCase() === 'yaml') || (exten.toLowerCase() === 'yml')) {
-    objectParsed = yaml.load(content);
-  }
-  return objectParsed;
+  return getParsedObject(content, exten);
 };
 
-export default getParsedObject;
+export default getParsedData;
