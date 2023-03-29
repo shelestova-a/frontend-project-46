93608b14e9c91314f014e8a14a815d6e6f3d435a
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-03-29 22:53:18 +0200

    modify reader.js

--- src/reader.js
+++ src/reader.js
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 
 const read = (filepath) => {
-  const pathAbsolute = path.resolve(filepath);
-  const content = fs.readFileSync(pathAbsolute, 'utf-8');
+  const getAbsolutePath = (fPath) => path.resolve(process.cwd(), fPath);
+  const content = fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
   return content;
 };
 
