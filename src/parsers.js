bf5676de66d0269b5bdc7ae781dee8308b4027e5
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-03-29 22:52:56 +0200

    modify parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,9 +2,9 @@ import yaml from 'js-yaml';
 
 const parse = (content, exten) => {
   let objectParsed;
-  if (exten.toLowerCase() === 'json') {
+  if (exten.toLowerCase() === '.json') {
     objectParsed = JSON.parse(content);
-  } else if ((exten.toLowerCase() === 'yaml') || (exten.toLowerCase() === 'yml')) {
+  } else if ((exten.toLowerCase() === '.yaml') || (exten.toLowerCase() === '.yml')) {
     objectParsed = yaml.load(content);
   }
   return objectParsed;
