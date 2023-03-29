b69e4b5da2becd1538da25c60bf4f06e2cc0d2b9
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-03-29 22:50:00 +0200

    add tests for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,8 +9,17 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const fileJSON1 = getFixturePath('file1.json');
 const fileJSON2 = getFixturePath('file2.json');
+
+const fileYAML1 = getFixturePath('file1.yaml');
+const fileYAML2 = getFixturePath('file2.yaml');
+
 const result = fs.readFileSync(getFixturePath('expected'), 'utf-8');
 
-test('genDiff', () => {
-  expect(genDiff(fileJSON1, fileJSON2)).toEqual(result);
+const testData = [
+  { file1: fileJSON1, file2: fileJSON2, expected: result },
+  { file1: fileYAML1, file2: fileYAML2, expected: result },
+];
+
+test.each(testData)('genDiff', ({ file1, file2, expected }) => {
+  expect(genDiff(file1, file2)).toEqual(expected);
 });
