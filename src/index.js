a3cab993b676ae4dadf956d32847017b59e08b63
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-03-29 22:52:02 +0200

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import path from 'path';
 import read from './reader.js';
-import parse from './parser.js';
+import parse from './parsers.js';
 import genDiff from './genDiff.js';
 
 export default (filepath1, filepath2) => {
-  const extension1 = filepath1.split('.').pop();
+  const extension1 = path.extname(filepath1);
   const objectParsed1 = parse(read(filepath1), extension1);
-  const extension2 = filepath2.split('.').pop();
+  const extension2 = path.extname(filepath2);
   const objectParsed2 = parse(read(filepath2), extension2);
   const result = genDiff(objectParsed1, objectParsed2);
   return result;
