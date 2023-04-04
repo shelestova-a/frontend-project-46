17781ae2d936c8537a2d61314e05f47a25d46ebe
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-04-04 16:31:52 +0200

    modify genDiff.js and index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import path from 'path';
 import read from './reader.js';
 import parse from './parsers.js';
 import genDiff from './genDiff.js';
+import makeStylish from './formatter/stylish.js';
 
-export default (filepath1, filepath2) => {
+export default (filepath1, filepath2, formatter = 'stylish') => {
   const extension1 = path.extname(filepath1);
   const objectParsed1 = parse(read(filepath1), extension1);
   const extension2 = path.extname(filepath2);
   const objectParsed2 = parse(read(filepath2), extension2);
   const result = genDiff(objectParsed1, objectParsed2);
+  if (formatter === 'stylish') {
+    return makeStylish(result);
+  }
   return result;
 };
