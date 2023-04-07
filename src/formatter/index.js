478c5c877d638dc0939b3e486bf8afb6466f6ae5
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-04-07 19:03:55 +0200

    add json.js

--- src/formatter/index.js
+++ src/formatter/index.js
@@ -1,11 +1,14 @@
 import getDiffTree from '../index.js';
 import makePlain from './plain.js';
 import makeStylish from './stylish.js';
+import makeJSON from './json.js';
 
 const getFormatted = (filepath1, filepath2, formatter = 'stylish') => {
   const diffTree = getDiffTree(filepath1, filepath2);
   if (formatter === 'plain') {
     return makePlain(diffTree);
+  } else if (formatter === 'json') {
+    return makeJSON(diffTree);
   }
   return makeStylish(diffTree);
 };
---
+++ src/formatter/json.js
@@ -0,0 +1,18 @@
+const makeJSON = (tree) => {
+  const iter = (value) => {
+    const result = value.reduce((acc, object) => {
+      const arr = Object.entries(object);
+      const str = arr.map(([key, val]) => {
+        if (key === 'children') {
+          return `"${key}":[${iter(val)}]`;
+        }
+        return `"${key}":${JSON.stringify(val)}`;
+      }).join(',');
+      return [...acc, `{${str}}`];
+    }, []).join(',');
+    return result;
+  };
+  return `{${iter(tree)}}`;
+};
+
+export default makeJSON;
