65651f2ad4fae1e1d7c40206197aecbefa9013c2
Anna Shelestova <anna.shelestova.1@gmail.com>
2023-04-07 19:23:11 +0200

    modify bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -15,5 +15,6 @@ program
   .action((filepath1, filepath2) => {
     const result = genDiff(filepath1, filepath2, program.opts().format);
     console.log(result);
-  })
-  .parse();
+  });
+
+program.parse();
